130cb130820a90345bfc6a0eb5ef1c5691f59d69
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2021-12-22 10:28:42 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,12 +1,58 @@
+def on_gesture_tilt_left():
+    basic.show_string("Merry Christmas and Happy New Year")
+    basic.show_string("Thank you for supporting this game ")
+    basic.show_string("Made by DanielY121")
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
 def on_button_pressed_a():
     Player.change(LedSpriteProperty.X, -1)
+    music.play_melody("C5 - - - - - - - ", 500)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_shake():
+    global Enemy, Player
+    Enemy.delete()
+    Player.delete()
+    music.play_melody("A B C5 D C F A F ", 500)
+    music.play_melody("C5 B A G A B C5 D ", 500)
+    for index in range(4):
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # # . # #
+                        # . . . #
+                        . # # # .
+        """)
+        basic.show_leds("""
+            . # # # .
+                        # # . # #
+                        # . . . #
+                        # . . . #
+                        . # # # .
+        """)
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # # . # #
+                        # . . . #
+                        . # # # .
+        """)
+        basic.show_leds("""
+            . # # # .
+                        # . . . #
+                        # . . . #
+                        # # . # #
+                        . # # # .
+        """)
+    Enemy = game.create_sprite(0, 0)
+    Player = game.create_sprite(2, 4)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 def on_button_pressed_ab():
     global Bullet, Enemy, Player
     Bullet = game.create_sprite(Player.get(LedSpriteProperty.X), 3)
     basic.pause(250)
-    for index in range(5):
+    for index2 in range(5):
         if Bullet.is_touching(Enemy):
             Enemy.delete()
             Bullet_from_enemy.delete()
@@ -27,7 +73,12 @@ input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     Player.change(LedSpriteProperty.X, 1)
+    music.play_melody("C5 - - - - - - - ", 500)
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_tilt_right():
+    basic.clear_screen()
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
 Enemy_speed = 0
 sprite = 0
@@ -40,7 +91,7 @@ Enemy = game.create_sprite(0, 0)
 
 def on_forever():
     global sprite, Enemy_speed, Bullet_from_enemy
-    for index2 in range(4):
+    for index3 in range(4):
         sprite = randint(0, 1)
         if sprite == 0:
             Enemy.change(LedSpriteProperty.X, 1)
@@ -55,17 +106,22 @@ def on_forever():
             Bullet_from_enemy = game.create_sprite(Enemy.get(LedSpriteProperty.X), 1)
             basic.pause(500)
             Enemy.change(LedSpriteProperty.X, 1)
-            for index3 in range(4):
+            for index4 in range(4):
                 Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
                 basic.pause(250)
                 if Bullet_from_enemy.is_touching(Player):
                     music.play_melody("E B C5 A B G A F ", 300)
                     music.play_melody("E - - - - - - - ", 103)
+                    basic.show_icon(IconNames.SMALL_HEART)
+                    basic.show_icon(IconNames.HEART)
+                    basic.show_icon(IconNames.SMALL_HEART)
+                    basic.show_string("Thank you for supporting this game.")
+                    basic.show_icon(IconNames.HAPPY)
                     basic.show_string("Made by DanielY121")
                     basic.pause(250)
                     game.game_over()
             Bullet_from_enemy.delete()
-    for index4 in range(4):
+    for index5 in range(4):
         sprite = randint(0, 1)
         if sprite == 0:
             Enemy.change(LedSpriteProperty.X, -1)
@@ -80,12 +136,17 @@ def on_forever():
             Bullet_from_enemy = game.create_sprite(Enemy.get(LedSpriteProperty.X), 1)
             basic.pause(500)
             Enemy.change(LedSpriteProperty.X, -1)
-            for index5 in range(4):
+            for index6 in range(4):
                 Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
                 basic.pause(250)
                 if Bullet_from_enemy.is_touching(Player):
                     music.play_melody("E B C5 A B G A F ", 300)
                     music.play_melody("E - - - - - - - ", 103)
+                    basic.show_icon(IconNames.SMALL_HEART)
+                    basic.show_icon(IconNames.HEART)
+                    basic.show_icon(IconNames.SMALL_HEART)
+                    basic.show_string("Thank you for supporting this game.")
+                    basic.show_icon(IconNames.HAPPY)
                     basic.show_string("Made by DanielY121")
                     basic.pause(250)
                     game.game_over()
