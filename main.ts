2c014a34f5c58c51eb1cff74a882c09eda005db8
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2022-01-07 13:18:37 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -140,9 +140,9 @@ input.onButtonPressed(Button.B, function () {
     music.playMelody("C5 - - - - - - - ", 500)
 })
 let Enemy_speed = 0
+let sprite = 0
 let EF1 = 0
 let Enemy_fever_1: game.LedSprite = null
-let sprite = 0
 let Bullet_from_enemy: game.LedSprite = null
 let Bullet: game.LedSprite = null
 let Enemy: game.LedSprite = null
@@ -155,94 +155,8 @@ loops.everyInterval(60000, function () {
 })
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
-        sprite = randint(0, 1)
-        if (sprite == 0) {
-            if (game.score() == 10) {
-                for (let index = 0; index < 5; index++) {
-                    Enemy_fever_1 = game.createSprite(4, 0)
-                    basic.pause(250)
-                    EF1 = randint(0, 4)
-                    if (EF1 == 0) {
-                        Enemy_fever_1.delete()
-                        Enemy_fever_1 = game.createSprite(0, 0)
-                        basic.pause(250)
-                        if (Bullet.isTouching(Enemy_fever_1)) {
-                            Enemy_fever_1.delete()
-                            game.addScore(1)
-                        }
-                    } else if (EF1 == 1) {
-                        Enemy_fever_1.delete()
-                        Enemy_fever_1 = game.createSprite(1, 0)
-                        basic.pause(250)
-                        if (Bullet.isTouching(Enemy_fever_1)) {
-                            Enemy_fever_1.delete()
-                            game.addScore(1)
-                        }
-                    } else if (EF1 == 2) {
-                        Enemy_fever_1.delete()
-                        Enemy_fever_1 = game.createSprite(2, 0)
-                        basic.pause(250)
-                        if (Bullet.isTouching(Enemy_fever_1)) {
-                            Enemy_fever_1.delete()
-                            game.addScore(1)
-                        }
-                    } else if (EF1 == 3) {
-                        Enemy_fever_1.delete()
-                        Enemy_fever_1 = game.createSprite(3, 0)
-                        basic.pause(250)
-                        if (Bullet.isTouching(Enemy_fever_1)) {
-                            Enemy_fever_1.delete()
-                            game.addScore(1)
-                        }
-                    } else {
-                        Enemy_fever_1.delete()
-                        Enemy_fever_1 = game.createSprite(4, 0)
-                        basic.pause(250)
-                        if (Bullet.isTouching(Enemy_fever_1)) {
-                            Enemy_fever_1.delete()
-                            game.addScore(1)
-                        }
-                    }
-                }
-            }
-            Enemy.change(LedSpriteProperty.X, 1)
-            Enemy_speed = randint(0, 2)
-            if (Enemy_speed == 0) {
-                basic.pause(100)
-            } else if (Enemy_speed == 1) {
-                basic.pause(250)
-            } else {
-                basic.pause(500)
-            }
-        } else {
-            Bullet_from_enemy = game.createSprite(Enemy.get(LedSpriteProperty.X), 1)
-            basic.pause(500)
-            Enemy.change(LedSpriteProperty.X, 1)
-            for (let index = 0; index < 4; index++) {
-                Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
-                basic.pause(250)
-                if (Bullet_from_enemy.isTouching(Player)) {
-                    Player.delete()
-                    Enemy.delete()
-                    music.playMelody("E B C5 A B G A F ", 300)
-                    music.playMelody("E - - - - - - - ", 103)
-                    basic.showIcon(IconNames.SmallHeart)
-                    basic.showIcon(IconNames.Heart)
-                    basic.showIcon(IconNames.SmallHeart)
-                    basic.showString("Thank you for supporting this game.")
-                    basic.showIcon(IconNames.Happy)
-                    basic.showString("Made by DanielY121")
-                    basic.pause(250)
-                    game.gameOver()
-                }
-            }
-            Bullet_from_enemy.delete()
-        }
-    }
-    for (let index = 0; index < 4; index++) {
-        sprite = randint(0, 1)
-        if (sprite == 0) {
-            if (game.score() == 10) {
+        if (game.score() == 10) {
+            for (let index = 0; index < 5; index++) {
                 Enemy_fever_1 = game.createSprite(4, 0)
                 basic.pause(250)
                 EF1 = randint(0, 4)
@@ -288,6 +202,92 @@ basic.forever(function () {
                     }
                 }
             }
+        }
+        sprite = randint(0, 1)
+        if (sprite == 0) {
+            Enemy.change(LedSpriteProperty.X, 1)
+            Enemy_speed = randint(0, 2)
+            if (Enemy_speed == 0) {
+                basic.pause(100)
+            } else if (Enemy_speed == 1) {
+                basic.pause(250)
+            } else {
+                basic.pause(500)
+            }
+        } else {
+            Bullet_from_enemy = game.createSprite(Enemy.get(LedSpriteProperty.X), 1)
+            basic.pause(500)
+            Enemy.change(LedSpriteProperty.X, 1)
+            for (let index = 0; index < 4; index++) {
+                Bullet_from_enemy.change(LedSpriteProperty.Y, 1)
+                basic.pause(250)
+                if (Bullet_from_enemy.isTouching(Player)) {
+                    Player.delete()
+                    Enemy.delete()
+                    music.playMelody("E B C5 A B G A F ", 300)
+                    music.playMelody("E - - - - - - - ", 103)
+                    basic.showIcon(IconNames.SmallHeart)
+                    basic.showIcon(IconNames.Heart)
+                    basic.showIcon(IconNames.SmallHeart)
+                    basic.showString("Thank you for supporting this game.")
+                    basic.showIcon(IconNames.Happy)
+                    basic.showString("Made by DanielY121")
+                    basic.pause(250)
+                    game.gameOver()
+                }
+            }
+            Bullet_from_enemy.delete()
+        }
+    }
+    for (let index = 0; index < 4; index++) {
+        if (game.score() == 10) {
+            Enemy_fever_1 = game.createSprite(4, 0)
+            basic.pause(250)
+            EF1 = randint(0, 4)
+            if (EF1 == 0) {
+                Enemy_fever_1.delete()
+                Enemy_fever_1 = game.createSprite(0, 0)
+                basic.pause(250)
+                if (Bullet.isTouching(Enemy_fever_1)) {
+                    Enemy_fever_1.delete()
+                    game.addScore(1)
+                }
+            } else if (EF1 == 1) {
+                Enemy_fever_1.delete()
+                Enemy_fever_1 = game.createSprite(1, 0)
+                basic.pause(250)
+                if (Bullet.isTouching(Enemy_fever_1)) {
+                    Enemy_fever_1.delete()
+                    game.addScore(1)
+                }
+            } else if (EF1 == 2) {
+                Enemy_fever_1.delete()
+                Enemy_fever_1 = game.createSprite(2, 0)
+                basic.pause(250)
+                if (Bullet.isTouching(Enemy_fever_1)) {
+                    Enemy_fever_1.delete()
+                    game.addScore(1)
+                }
+            } else if (EF1 == 3) {
+                Enemy_fever_1.delete()
+                Enemy_fever_1 = game.createSprite(3, 0)
+                basic.pause(250)
+                if (Bullet.isTouching(Enemy_fever_1)) {
+                    Enemy_fever_1.delete()
+                    game.addScore(1)
+                }
+            } else {
+                Enemy_fever_1.delete()
+                Enemy_fever_1 = game.createSprite(4, 0)
+                basic.pause(250)
+                if (Bullet.isTouching(Enemy_fever_1)) {
+                    Enemy_fever_1.delete()
+                    game.addScore(1)
+                }
+            }
+        }
+        sprite = randint(0, 1)
+        if (sprite == 0) {
             Enemy.change(LedSpriteProperty.X, -1)
             Enemy_speed = randint(0, 2)
             if (Enemy_speed == 0) {
